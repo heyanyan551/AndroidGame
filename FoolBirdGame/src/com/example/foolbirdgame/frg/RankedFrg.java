4a4186ed152246618d1a72dc78989ac40b20a51f
heyanyan551 <963316026@qq.com>
2014-11-08 23:09:09 +0800

    ad

--- FoolBirdGame/src/com/example/foolbirdgame/frg/RankedFrg.java
+++ FoolBirdGame/src/com/example/foolbirdgame/frg/RankedFrg.java
@@ -1,5 +1,6 @@
 package com.example.foolbirdgame.frg;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -15,21 +16,30 @@ import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.squareup.picasso.Picasso;
 
 import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class RankedFrg extends Fragment {
 	private List<HashMap<String, String>> rankeAdView;
 	private HashMap<String, String> map;
-    TextView  ad_left;
-    TextView  ad_right;
+    ImageView  ad_left;
+    ImageView  ad_right;
+    LinearLayout  ad;
 	private View view;
+	private int screenWidth;
+	private int screenHeigt;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -41,14 +51,25 @@ public class RankedFrg extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		WindowManager manager = (WindowManager) getActivity()
+				.getSystemService(getActivity().WINDOW_SERVICE);
+		DisplayMetrics dm = new DisplayMetrics();
+		manager.getDefaultDisplay().getMetrics(dm);
+		screenHeigt=(dm.heightPixels);
 		rankeView();
-		init();
+		initad();
 	}
 
 	
-	private void init() {
-		
+	private void initad() {
+		ad=(LinearLayout) view.findViewById(R.id.ranke_ad);
+		LayoutParams params = (LayoutParams) ad.getLayoutParams();
+		params.height =screenHeigt *3/10;
+		ad.setLayoutParams(params);
+		ad_left= (ImageView) view.findViewById(R.id.adleft);
+		ad_right=(ImageView) view.findViewById(R.id.adlight);
 	}
+
 
 	private void rankeView() {
 		String path = Constant.RANKE_AD_URL;
@@ -65,6 +86,7 @@ public class RankedFrg extends Fragment {
 	}
 
 	private void jsonRandead(String date) {
+		rankeAdView=new ArrayList<HashMap<String,String>>();
          try {
 			JSONObject   rankeads=new JSONObject(date);
 			JSONArray   arrryad=rankeads.getJSONArray("items");
@@ -77,7 +99,8 @@ public class RankedFrg extends Fragment {
 				map.put("gameid", rankeadsitem.getString("gameid"));
 				rankeAdView.add(map);
 			}
-			System.out.println(arrryad);
+			Picasso.with(getActivity()).load(Constant.BOOT_URL+rankeAdView.get(0).get("titlepic")).into(ad_left);
+			Picasso.with(getActivity()).load(Constant.BOOT_URL+rankeAdView.get(1).get("titlepic")).into(ad_right);
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
